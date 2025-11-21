6b959f6af06f78ae6c23fabb3c26a901c7b78817
{JosephNgugi254} <solitaryjoe069@gmail.com>
2025-11-21 21:57:50 +0300

    updated process payment amount temporarily

--- frontend/src/pages/AdminPanel.jsx
+++ frontend/src/pages/AdminPanel.jsx
@@ -2482,6 +2482,8 @@ function AdminPanel() {
                 className="form-control" 
                 id="paymentAmount" 
                 name="amount" 
+                min={selectedClient.balance < 1 ? undefined : 1}
+                max={selectedClient.balance < 1 ? undefined : Math.floor(selectedClient.balance || 0)}
                 // min="0.01"
                 // max={Math.floor(selectedClient.balance || 0)}
                 placeholder="Enter amount"
